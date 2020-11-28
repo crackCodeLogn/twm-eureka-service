f0c2694e15cc67091aa1bb48c312fda96d458188
Vivek <vivektheintel@gmail.com>
2020-11-28 11:29:57 +0530

    Upgrading startup script

--- bin/twmEureaka.sh
+++ bin/twmEureaka.sh
@@ -1,5 +1,9 @@
-VERSION="1.0-SNAPSHOT"
-BASE_PATH=$TWM_HOME_PARENT/TWM/twm-eureka-service/bin     #TWM-HOME-PARENT :: exported in .bashrc
-JAR_PATH=$BASE_PATH/../target/twm-eureka-service-$VERSION.jar
+APP_NAME="TWM-EUREKA-SERVICE"
+APP_VERSION="1.0-SNAPSHOT"
+JAVA_PARAM="-Xmx251m"
 
-java -Xmx251m -jar $JAR_PATH
+BASE_PATH=$TWM_HOME_PARENT/TWM/twm-eureka-service/bin     #TWM-HOME-PARENT :: exported in .bashrc
+JAR_PATH=$BASE_PATH/../target/twm-eureka-service-$APP_VERSION.jar
+
+echo "Starting '$APP_NAME' with java param: '$JAVA_PARAM', at '$JAR_PATH'"
+java $JAVA_PARAM -jar $JAR_PATH
