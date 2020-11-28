e1d4115c725adb82ee57a52ea74166a79a6e55c0
Vivek <vivektheintel@gmail.com>
2020-11-28 11:37:24 +0530

    Removing hard-coded path name

--- bin/twm-eureka.sh
+++ bin/twm-eureka.sh
@@ -1,9 +1,9 @@
-APP_NAME="TWM-EUREKA-SERVICE"
+APP_NAME="twm-eureka-service"
 APP_VERSION="1.0-SNAPSHOT"
 JAVA_PARAM="-Xmx251m"
 
-BASE_PATH=$TWM_HOME_PARENT/TWM/twm-eureka-service/bin     #TWM-HOME-PARENT :: exported in .bashrc
-JAR_PATH=$BASE_PATH/../target/twm-eureka-service-$APP_VERSION.jar
+BIN_PATH=$TWM_HOME_PARENT/TWM/$APP_NAME/bin     #TWM-HOME-PARENT :: exported in .bashrc
+JAR_PATH=$BIN_PATH/../target/$APP_NAME-$APP_VERSION.jar
 
 echo "Starting '$APP_NAME' with java param: '$JAVA_PARAM', at '$JAR_PATH'"
 java $JAVA_PARAM -jar $JAR_PATH
